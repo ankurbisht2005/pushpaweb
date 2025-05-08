c0a10b09fe2f7bbfe8bc2e522554dc37507c91ae
ankurbisht2005 <ankurbisht799@gmail.com>
2025-05-08 10:01:56 +0530

    All okssss

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
   return (
     <>
     <h1>This is Computer Guru</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, eos accusamus quae magnam culpa sed eaque ab voluptates repellendus consequuntur accusantium suscipit dignissimos, sequi necessitatibus odio labore repellat! Eum, sit!
+    </p>
     </>
   )
 }
